8fc1bba74ead1b03c2ebdae7e5cc65d72cb3255c
sundayjoa <jeonjoo1004@gmail.com>
2024-07-23 13:58:59 +0900

    회원가입/로그인 기능 구현

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,75 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController{
+	
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
+		
+		try {
+			if(userDTO == null || userDTO.getPassword() == null) {
+				throw new RuntimeException("Invalid Password value.");
+			}
+			
+			//저장할 유저 만들기
+			UserEntity user = UserEntity.builder()
+					.username(userDTO.getUsername())
+					.password(userDTO.getPassword())
+					.build();
+			
+			UserEntity registeredUser = userService.create(user);
+			UserDTO responseUserDTO = UserDTO.builder()
+					.id(registeredUser.getId())
+					.username(registeredUser.getUsername())
+					.build();
+			
+			return ResponseEntity.ok().body(responseUserDTO);
+		} catch (Exception e) {
+			ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+			return ResponseEntity
+					.badRequest()
+					.body(responseDTO);
+		}
+	}
+	
+	@PostMapping("/signin")
+	public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO){
+		UserEntity user = userService.getByCredentials(
+				userDTO.getUsername(),
+				userDTO.getPassword());
+		
+		if(user != null) {
+			final UserDTO responseUserDTO = UserDTO.builder()
+					.username(user.getUsername())
+					.id(user.getId())
+					.build();
+			return ResponseEntity.ok().body(responseUserDTO);
+			} else {
+				ResponseDTO responseDTO = ResponseDTO.builder()
+						.error("Login failed.")
+						.build();
+				return ResponseEntity
+						.badRequest()
+						.body(responseDTO);
+			}
+		}
+}
